2ae555f7b095a5378a395ae82c8d959b5a246cbb
Adam N <76851495+JhonyLemon@users.noreply.github.com>
2022-07-28 09:07:33 +0200

    Initial commit

---
+++ src/main/java/pl/fissst/lbd/queues/QueuesApplication.java
@@ -0,0 +1,13 @@
+package pl.fissst.lbd.queues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueuesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/fissst/lbd/queues/QueuesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fissst.lbd.queues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueuesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
